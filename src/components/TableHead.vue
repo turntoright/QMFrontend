66bc1e6c8bc7bf0cc12175f609e0f5a4fd6a934f
turntoright <hongwucao1975@gmail.com>
2021-05-08 22:56:00 +1200

    New feature: table head buttons

---
+++ src/components/TableHead.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="15" :offset="1">
+        <el-link type="primary">Send</el-link>
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary">Print</el-link>
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary">Delete</el-link>
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary">Accept</el-link>
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary">Decline</el-link>
+      </el-col>
+      <el-col :span="4">
+        <p class="info">6 items, 1,724.15</p>
+      </el-col>
+      <el-col :span="1">
+        <p class="nzd info">NZD</p>
+      </el-col>
+      <el-col :span="3">
+        <el-divider direction="vertical"></el-divider>
+        <el-link type="primary">Search</el-link>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TableHead",
+};
+</script>
+
+<style>
+.nzd {
+  color: dodgerblue;
+}
+.info {
+  font-size: 15px;
+}
+</style>
